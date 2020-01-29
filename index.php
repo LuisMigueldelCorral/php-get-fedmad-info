b302d39c818d7084881a5b1ddbb1426806016cfc
Luis Miguel del Corral Cuervo <luisdelcorral@ADAMS.ES>
2020-01-29 13:20:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
 function file_get_contents_utf8($fn) { 
 	 $content = file_get_contents($fn); 
-	  return mb_convert_encoding($content, 'UTF-8', 
-		  mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true)); 
+	  return $content
 }
 
 // Crear un flujo
