31ffbb31848a021512b8acd997d441ee73092a23
Luis Miguel del Corral Cuervo <luisdelcorral@ADAMS.ES>
2020-01-29 13:33:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+
+function curl_get_contents($url)
+{
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $url);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+   $html = curl_exec($ch);
+   $data = curl_exec($ch);
+   curl_close($ch);
+   return $data;
+}
+
 // Crear un flujo
 $opciones = array(
   'http'=>array(
@@ -11,7 +24,7 @@ $opciones = array(
 $contexto = stream_context_create($opciones);
 
 // Abre el fichero usando las cabeceras HTTP establecidas arriba
-$fichero = file_get_contents('http://www.fedmadtm.com/', false, $contexto);
+$fichero = curl_get_contents('http://www.fedmadtm.com/', false, $contexto);
 
 echo $fichero;
 
