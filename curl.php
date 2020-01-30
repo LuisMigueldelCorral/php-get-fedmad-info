dc3dd0554a4bcbe602e3e000ebb9d0a16ed0e051
Luis Miguel del Corral Cuervo <luisdelcorral@ADAMS.ES>
2020-01-30 14:46:01 +0100

    Update curl.php

--- curl.php
+++ curl.php
@@ -1,31 +1,11 @@
 <?php
 
 $url = 'http://www.example.com';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
-curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_TIMEOUT,10);
-$output = curl_exec($ch);
-$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-
-echo 'HTTP code: ' . $httpcode;
-
 
 $url = 'http://www.fedmadtm.com';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
-curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_TIMEOUT,10);
-$output = curl_exec($ch);
-$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-
-echo 'HTTP code: ' . $httpcode;
 
 $url = 'http://www.rfetm.es';
+
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
 curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
