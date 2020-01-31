ce56f3f700c57b966947e44afeb640de945cc047
Luis Miguel del Corral Cuervo <luisdelcorral@ADAMS.ES>
2020-01-31 13:03:06 +0100

    Update rfetm.php

--- rfetm.php
+++ rfetm.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Noticias RFETM</title>
+</head>
+
+<body>
+
 <?php
 
 function curl_get_contents($url)
@@ -31,3 +40,7 @@ $fichero = '<div id="slider" class="nivoSlider">' . $fichero
 echo $fichero;
 
 ?>
+
+
+</body>
+</html>
